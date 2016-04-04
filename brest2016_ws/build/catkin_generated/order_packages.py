a615def67c1a83efd9fb0f2f43b44661ac7203c4
brest2016 <ejalaa12@gmail.com>
2016-04-04 10:27:50 +0200

    First Commit - Initialisation du workspace

---
+++ brest2016_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/brest2016/Desktop/Brest 2016/brest-git/Brest2016/brest2016_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
---
+++ brest2016_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/brest2016/Desktop/Brest 2016/brest-git/Brest2016/brest2016_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/brest2016/Desktop/Brest 2016/brest-git/Brest2016/brest2016_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/brest2016/Desktop/Brest 2016/brest-git/Brest2016/brest2016_ws/devel/lib:/home/brest2016/Desktop/Brest 2016/brest-git/Brest2016/brest2016_ws/devel/lib/x86_64-linux-gnu:/home/brest2016/Desktop/Brest 2016/brest2016_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/brest2016/Desktop/Brest 2016/brest2016_ws/devel/lib:/opt/ros/indigo/lib"
+export PATH="/home/brest2016/Desktop/Brest 2016/brest-git/Brest2016/brest2016_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/brest2016/Desktop/Brest 2016/brest-git/Brest2016/brest2016_ws/devel/lib/pkgconfig:/home/brest2016/Desktop/Brest 2016/brest-git/Brest2016/brest2016_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/home/brest2016/Desktop/Brest 2016/brest2016_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/brest2016/Desktop/Brest 2016/brest2016_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/brest2016/Desktop/Brest 2016/brest-git/Brest2016/brest2016_ws/build"
+export PYTHONPATH="/home/brest2016/Desktop/Brest 2016/brest-git/Brest2016/brest2016_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/brest2016/Desktop/Brest 2016/brest-git/Brest2016/brest2016_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/brest2016/Desktop/Brest 2016/brest-git/Brest2016/brest2016_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
