f4982e6369da25456c1f238713b4469bed5bbc89
ejalaa12 <ejalaa12@gmail.com>
2016-04-05 17:51:52 +0200

    Automatic camera path

--- brest2016_ws/src/sensors/src/Camera_publisher.py
+++ brest2016_ws/src/sensors/src/Camera_publisher.py
@@ -3,6 +3,7 @@ import rospy
 from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
+import glob
 
 # Initiate node
 rospy.init_node('video_capture')
@@ -11,7 +12,9 @@ rospy.init_node('video_capture')
 rate = rospy.Rate(10)
 
 # Camera path
-cam_path = '/dev/video1'
+cam_path = glob.glob('/dev/video*')[0]
+index = int(cam_path[-1])
+print index
 
 # Bridge object
 bridge = CvBridge()
@@ -20,7 +23,7 @@ bridge = CvBridge()
 pub = rospy.Publisher('camera/image_raw', Image, queue_size=1)
 
 # Capture object
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(index)
 
 if not cap.isOpened():
     print 'Error while opening: ' + cam_path
@@ -36,4 +39,7 @@ while rval:
         pub.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
     except CvBridgeError as e:
         print e
+    except KeyboardInterrupt as e:
+        cap.release()
+        print 'camera released'
     rate.sleep()
