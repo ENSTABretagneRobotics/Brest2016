90dd8fa4be21af7b20e181b2202b3ed114592f3a
ejalaa12 <ejalaa12@gmail.com>
2016-04-11 17:48:58 +0200

    AIS publisher (Ajout du decodage des messages de 2 lignes)

--- brest2016_ws/src/sensors/src/AISVDM_publisher.py
+++ brest2016_ws/src/sensors/src/AISVDM_publisher.py
@@ -12,29 +12,41 @@ pub = rospy.Publisher('AISVDM', AISVDM)
 rate = rospy.Rate(2)
 ser = serial.Serial("/dev/ttyACM0")
 
-# with open("/dev/ttyACM0") as f:
-#     for msg in ais.stream.decode(f):
-#         print msg
 
+def publish(data):
+    msg = AISVDM()
+    msg.heure = int(data['utc_hour'])
+    msg.minute = int(data['utc_min'])
+    msg.numMMSI = int(data['mmsi'])
+    msg.latitude = float(data['x'])
+    msg.longitude = float(data['y'])
+    msg.vitesse = float(data['sog'])
+    msg.cap = float(data['cog'])
+    msg.vitesseRot = float(data['rot'])
+    msg.statut = float(data['special_manoeuvre'])
+
+    print data
+    pub.publish(msg)
+    rate.sleep()
+
+buff = ''  # for first part of 2-lines msgs
 while not rospy.is_shutdown():
 
     data = ser.readline()
     data = data.split(',')
-    if (data[0] == '!AIVDM' and data[1] == 1):
+    # msg in one line
+    if data[0] == '!AIVDM' and data[1] == '1':
+        publish(ais.decode(data[5], 0))
 
-        data = ais.decode(data[5])
+    # msg in two lines, part 1
+    elif data[0] == '!AIVDM' and data[1] == '2' and data[2] == '1':
+        buff = data[5]
 
-        msg = AISVDM()
-        msg.heure = data['utc_hour']
-        msg.minute = data['utc_minut']
-        msg.numMMSI = data['mmsi']
-        msg.latitude = data['x']
-        msg.longitude = data['y']
-        msg.vitesse = data['sog']
-        msg.cap = data['cog']
-        msg.vitesseRot = data['rot']
-        msg.statut = data['special_manoeuvre']
-
-        print data
-        pub.publish(msg)
-    rate.sleep()
+    # msg in two lines, part 2
+    elif data[0] == '!AIVDM' and data[1] == '2' and data[2] == '2':
+        if len(buff) == 56 and len(data[5]) == 15:
+            total = buff + data[5]
+            publish(ais.decode(total, 2))
+            buff = ''
+        else:
+            publish('error', 'biz')
