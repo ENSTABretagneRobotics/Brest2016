c0eba6a9c3e1292aeb167b4545d8749a5e11ed90
ejalaa12 <ejalaa12@gmail.com>
2016-05-13 17:49:30 +0200

    ajout de la classe simulatin

---
+++ brest2016_ws/src/simulation2d/src/sim_char.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Vector3, PoseStamped
+import tf
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.linalg import norm
+
+
+def draw_tank(x):
+    M = np.array([[1, -1, 0, 0, -1, -1, 0, 0, -1, 1, 0, 0, 3, 3, 0],
+                  [-2, -2, -2, -1, -1, 1, 1, 2, 2, 2, 2, 1, 0.5, -0.5, -1]])
+    M = np.vstack((M, np.ones(M.shape[1])))
+    R = np.array([[np.cos(x[2]), -np.sin(x[2]), x[0]],
+                  [np.sin(x[2]), np.cos(x[2]), x[1]],
+                  [0, 0, 1]])
+
+    M = np.dot(R, M)
+    plt.plot(M[0], M[1])
+
+
+def fdot(x, u):
+    xdot = u[1] * np.cos(x[2])
+    ydot = u[1] * np.sin(x[2])
+    thetadot = u[0]
+    return np.array([xdot, ydot, thetadot])
+
+
+def update_cmd(msg):
+    global u, x
+    vect = np.array([msg.x, msg.y])
+    u[1] = norm(vect)
+    thetabar = np.arctan2(msg.y, msg.y)
+    u[2] = 10 * np.arctan(np.tan(0.5 * (thetabar - x[2])))
+
+
+def publish_pose(x):
+    pos = PoseStamped()
+    pos.header.frame_id = 'world'
+    pos.header.stamp = rospy.Time.now()
+    pos.pose.position.x = x[0]
+    pos.pose.position.y = x[1]
+    pos.pose.position.z = 0
+
+    quaternion = tf.transformations.quaternion_from_euler(0, 0, x[2])
+    pos.pose.orientation.x = quaternion[0]
+    pos.pose.orientation.y = quaternion[1]
+    pos.pose.orientation.z = quaternion[2]
+    pos.pose.orientation.w = quaternion[3]
+
+    pos_pub.publish(pos)
+
+# Initialisation du noeud
+rospy.init_node('service_server')
+# Subscriber et publisher
+cmd_sub = rospy.Subscriber('robot/vecteur_cible', Vector3, update_cmd)
+pos_pub = rospy.Publisher('gps/local_pose', PoseStamped, queue_size=1)
+
+rate = rospy.Rate(10)
+plt.ion()
+x = np.array([-20, -10, 4])
+dt = 0.1
+u = [0, 1]
+
+while not rospy.is_shutdown():
+    plt.cla()
+    x = x + fdot(x, u) * dt
+    draw_tank(x)
+    plt.axis([-30, 30, -30, 30])
+    plt.draw()
+    publish_pose(x)
+
+
+# if __name__ == '__main__':
+#     for t in np.arange(1, 30, dt):
+#         plt.cla()
+#         x = x + fdot(x, u) * dt
+#         draw_tank(x)
+#         plt.axis([-30, 30, -30, 30])
+#         plt.draw()
