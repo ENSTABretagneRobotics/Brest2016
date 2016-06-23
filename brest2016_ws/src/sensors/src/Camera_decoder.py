741d6fa194cc8e2b0daf0d21328ea10ea192acd7
ejalaa12 <ejalaa12@gmail.com>
2016-06-23 16:27:34 +0200

    encoding/decoding image nodes

---
+++ brest2016_ws/src/sensors/src/Camera_decoder.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+
+# publie la video filmee par la camera
+
+
+import rospy
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+
+
+def update_image(msg):
+    global frame, bridge, flag
+    frame = bridge.imgmsg_to_cv2(msg, "mono8")
+    if not flag:
+        flag = True
+
+# Initiate node
+rospy.init_node('video_decoding')
+
+# Node rate
+rate = rospy.Rate(10)
+
+# Bridge object
+bridge = CvBridge()
+
+# Subscriber to compressed image
+sub = rospy.Subscriber('camera/image_compressed', Image, update_image)
+# Publisher
+pub = rospy.Publisher('camera/image_uncompressed', Image, queue_size=1)
+
+frame = np.ndarray(0)
+flag = False
+# Publishing the frame
+while not rospy.is_shutdown():
+    if flag:
+        frame2 = cv2.imdecode(frame, 1)
+        # print type(frame2)
+        try:
+            pub.publish(bridge.cv2_to_imgmsg(frame2, "bgr8"))
+        except CvBridgeError as e:
+            print e
+    rate.sleep()
--- brest2016_ws/src/sensors/src/Camera_publisher.py
+++ brest2016_ws/src/sensors/src/Camera_publisher.py
@@ -22,7 +22,7 @@ cam_path = '/dev/video1'
 bridge = CvBridge()
 
 # Publisher
-pub = rospy.Publisher('camera/image_raw', Image, queue_size=1)
+pub = rospy.Publisher('camera/image_compressed', Image, queue_size=1)
 
 # Capture object
 cap = cv2.VideoCapture(1)
@@ -37,10 +37,10 @@ else:
 rval, frame = cap.read()
 while rval:
     rval, frame = cap.read()
-    r,frame2=cv2.imencode('.jpeg', frame, [cv2.IMWRITE_JPEG_QUALITY, 70])
+    r, frame2 = cv2.imencode('.jpeg', frame, [cv2.IMWRITE_JPEG_QUALITY, 70])
     try:
-        pub.publish(bridge.cv2_to_imgmsg(frame2, "bgr8"))
-        #cv2.imdecode(frame2,1) à rajouter en sortie
+        pub.publish(bridge.cv2_to_imgmsg(frame2, "mono8"))
+        # cv2.imdecode(frame2,1) a rajouter en sortie
     except CvBridgeError as e:
         print e
     rate.sleep()
